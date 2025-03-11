36f209ed17cfc20c9885059976dc868e3d4b7d04
juyeonBae <w2316@e-mirim.hs.kr>
2025-03-11 15:39:37 +0900

    arrow function 2, 3

--- Grammer/function.js
+++ Grammer/function.js
@@ -12,6 +12,16 @@ anonymous('swag')
 
 //화살표 함수
 arrow = (word) => {
-    console.log("화살표 함수 " + word);
+    console.log("화살표 함수1 " + word);
 }
+arrow("arrow function")
+
+//하나일땐 괄호 생략 가능
+arrow = word => {
+    console.log("화살표 함수 축약2 " + word);
+}
+arrow("arrow function")
+
+//함수 내용이 한줄일땐 중괄호 생략 가능
+arrow = word =>  console.log("화살표 함수3 " + word);
 arrow("arrow function")
