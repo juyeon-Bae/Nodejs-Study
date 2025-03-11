8b17e1ff3183cf2c6ee7d43db13e9d79a7afb007
juyeonBae <w2316@e-mirim.hs.kr>
2025-03-11 15:17:37 +0900

    01. 익명함수

---
+++ Grammer/function.js
@@ -0,0 +1,11 @@
+function test(word){
+    console.log("test함수 " + word);
+}
+
+test("Hello")
+
+//익명함수
+anonymous  = function(word){
+    console.log("익명함수 " + word);
+}
+anonymous('swag')
--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-// http 모듈을 가져옵니다.
-const http = require('http');
+// express 모듈을 가져옵니다.
+const express = require('express');
 
-// 서버를 생성합니다.
-const server = http.createServer((req, res) => {
-  // HTTP 응답 헤더를 설정합니다.
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+// express 애플리케이션 객체를 생성합니다.
+const app = express();
 
-  // 응답 본문을 작성합니다.
-  res.end('Hello, World!\n');
+// 루트 경로에 대한 GET 요청을 처리합니다.
+app.get('/', (req, res) => {
+  // 클라이언트에게 'Hello, World!' 메시지를 보냅니다.
+  res.send('Hello, World!');
 });
 
-// 서버가 포트 3000에서 요청을 기다리게 합니다.
-server.listen(3000, 'localhost', () => {
+// 서버가 3000번 포트에서 요청을 기다리게 합니다.
+app.listen(3000, () => {
   console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
 });
-    
