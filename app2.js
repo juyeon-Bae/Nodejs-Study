67300f5375ce8ca8e46a82d833300dbdba70e4ff
juyeonBae <w2316@e-mirim.hs.kr>
2025-04-01 15:04:37 +0900

    get READ 완성

--- app2.js
+++ app2.js
@@ -46,6 +46,28 @@ app.get('/travel', (req, res)=>{
     })
 });
 
+//여행지 상세 페이지 
+app.get('/travel/:id', (req, res)=>{
+    const travelId = req.params.id;//req.params.id를 통해 URL에서 전달된 여행지 ID를 가져옴
+    const _query = 'SELECT * FROM travelList WHERE id =?'; //travelID를 사용하여 travelList 테이블에서 해당 여행지 정보를 조회하는 쿼리문
+ 
+    db.query(_query, [travelId], (err, results)=>{ //여행지 ID를 쿼리문에 전달하여 해당 여행지 정보를 가져옴
+        if(err){
+            console.error('데이터베이스 쿼리 실패: ', err);
+            res.status(500).send('내부 서버 에러');
+            return;
+        }
+
+        //여행지 정보가 없을 경우 처리
+        if(results.length === 0){
+            res.status(404).send('여행지 정보를 찾을 수 없습니다.'); //404(서버에서 발생하지 않아서)
+            return;
+        }
+        const travel = results[0]; //여행지 정보가 존재할 경우, 첫 번째 결과를 travel 변수에 저장
+        res.render('travelDetail', { travel }); //travel 변수를 템플릿에 전달하여 여행지 상세 페이지를 렌더링   
+    })
+})
+
 
 app.use((req,res)=>{
 
