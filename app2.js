abb4581270c0178b711d5ff813f828a7ac8c5148
juyeonBae <w2316@e-mirim.hs.kr>
2025-04-08 15:02:03 +0900

    Put(Update) 백엔드

--- app2.js
+++ app2.js
@@ -92,6 +92,28 @@
         res.render('addTravel');
     })
 
+    app.put('/travel/:id', (req, res)=>{
+        const travelId = req.params.id;//req.params.id를 통해 URL에서 전달된 여행지 ID를 가져옴
+        const {name} = req.body;
+        const _query = 'UPDATE travelList SET name = ? WHERE id = ?';  
+    
+        db.query(_query, [name, travelId], (err, results)=>{ //여행지 ID를 쿼리문에 전달하여 해당 여행지 정보를 가져옴, travleId(sql 인젝션 방지 ?)
+            if(err){
+                console.error('데이터베이스 쿼리 실패: ', err);
+                res.status(500).send('내부 서버 에러');
+                return;
+            }
+
+            //여행지 정보가 없을 경우 처리
+            if(results.length === 0){
+                res.status(404).send('여행지 정보를 찾을 수 없습니다.');  
+                return;
+            }
+            const travel = results[0]; //여행지 정보가 존재할 경우, 첫 번째 결과를 travel 변수에 저장
+            res.render('updateSuccess', { travel });  
+        })
+    })
+        
     app.use((req,res)=>{
         
     })
