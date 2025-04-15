9855efd616283f562c17ef71f5499a1f04252de8
juyeonBae <w2316@e-mirim.hs.kr>
2025-04-15 14:50:01 +0900

    createPool

--- db.js
+++ db.js
@@ -3,19 +3,19 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database:process.env.DB_NAME
 })
 //db연결
-db.connect(error => {
-    if(error){
-        console.error('MySQL 연결 실패')
-    }else{
-        console.log('MySQL 연결 성공')
-    }
-})
+// db.connect(error => {
+//     if(error){
+//         console.error('MySQL 연결 실패')
+//     }else{
+//         console.log('MySQL 연결 성공')
+//     }
+// })
 
 module.exports = db;
