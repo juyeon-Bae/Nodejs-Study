f489fa0321b37652d059dd2f5393389f06330507
juyeonBae <w2316@e-mirim.hs.kr>
2025-04-17 10:20:02 +0900

    promise()로 수정

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = mysql.createPool({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
@@ -17,5 +17,7 @@ const db = mysql.createPool({
 //         console.log('MySQL 연결 성공')
 //     }
 // })
+
+const db = pool.promise();
 
 module.exports = db;
